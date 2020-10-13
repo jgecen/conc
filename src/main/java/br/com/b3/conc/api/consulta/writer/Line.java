61c5ff64542d4bd89f8e33102cc6180a1139c630
Gecen <jgecen@gmail.com>
2020-10-13 01:39:08 -0300

    Testes e Linha do arquivo

--- src/main/java/br/com/b3/conc/api/consulta/writer/Line.java
+++ src/main/java/br/com/b3/conc/api/consulta/writer/Line.java
@@ -2,13 +2,15 @@ package br.com.b3.conc.api.consulta.writer;
 
 public class Line {
 
-	private String[] values;
+	private String[] values = { 
+			"--", ";--", ";--", ";--", ";--", 
+			";--", ";--", ";--", ";--", ";--", 
+			";--", ";--", ";--", ";--", ";--" };
 
 	public Line() {
 		super();
-		values = new String[15];
-	}
 
+	}
 
 	@Override
 	public String toString() {
@@ -22,7 +24,7 @@ public class Line {
 
 	public void addField(int i, String value) {
 		if(value == null) {
-			value = "--";
+			return;
 		}
 		if (i == 0) {
 			values[i] = value;
--- src/main/java/br/com/b3/conc/api/consulta/writer/ModelToLine.java
+++ src/main/java/br/com/b3/conc/api/consulta/writer/ModelToLine.java
@@ -3,7 +3,6 @@ package br.com.b3.conc.api.consulta.writer;
 import br.com.b3.conc.api.consulta.models.rest.Boleto;
 import br.com.b3.conc.api.consulta.models.rest.Operacao;
 import br.com.b3.conc.api.consulta.models.rest.Resultado;
-import br.com.b3.conc.api.consulta.models.shared.enums.TipoOrigemEnum;
 
 public class ModelToLine {
 
@@ -21,14 +20,7 @@ public class ModelToLine {
 			line.addField(5, dadosBoleto.getCodigoLinhaDigitavel());
 			line.addField(6, toString(dadosBoleto.getValorBoleto()));
 			line.addField(13, toString(dadosBoleto.getTipoServicoContratado()));			
-		} else {
-			line.addField(1, null);
-			line.addField(2, null);
-			line.addField(4, null);		
-			line.addField(5, null);
-			line.addField(6, null);
-			line.addField(13, null);
-		}
+		} 
 			
 		if (dadosOperacao != null) {
 			line.addField(3, toString(dadosOperacao.getTipoOrigemTaxa()));	
@@ -38,15 +30,7 @@ public class ModelToLine {
 			line.addField(10, toString(dadosOperacao.getDataCadastro()));	
 			line.addField(11, toString(dadosOperacao.getValorOperacao()));
 			line.addField(12, toString(dadosOperacao.getValorFaturado()));			
-		} else {
-			line.addField(3, null);	
-			line.addField(7, null);		
-			line.addField(8, null);
-			line.addField(9, null);
-			line.addField(10, null);	
-			line.addField(11, null);
-			line.addField(12, null);			
-		}
+		} 
 		
 		line.addField(14, resultado.getResultadoBatimento());		
 		return line;
