d6b86a284e89daedf5a1e9d1609a3c72e84febbc
Gecen <jgecen@gmail.com>
2020-10-22 00:21:48 -0300

    Banco de dados

--- src/main/java/br/com/b3/conc/ConcApplication.java
+++ src/main/java/br/com/b3/conc/ConcApplication.java
@@ -1,13 +1,38 @@
 package br.com.b3.conc;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 
 @SpringBootApplication
 public class ConcApplication {
 
+	@Value("${datasource.username}")
+	String username;
+
+	@Value("${datasource.password}")
+	String password;
+
+	@Value("${datasource.jdbcUrl}")
+	String jdbcUrl;
+
 	public static void main(String[] args) {
 		SpringApplication.run(ConcApplication.class, args);
+	}
+
+	@Bean
+	@Primary
+	public DataSource dataSource() {
+		System.out.println("username: " + username);
+		System.out.println("password: " + password);
+		System.out.println("jdbcUrl: " + jdbcUrl);
+		return DataSourceBuilder.create().password(password + "gres").url(jdbcUrl).username(username + "gres").build();
 	}
 
 }
